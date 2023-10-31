2c82c183abf92cdfd1b1b641bfb30e3d7b568427
HarrytheCake <96984868+HarrytheCake@users.noreply.github.com>
2023-10-31 20:04:14 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import discord
+import time
+import asyncio
+import os
+
+try:
+    DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
+except KeyError:
+    DISCORD_TOKEN = "Token not available!"
+    #logger.info("Token not available!")
+    #raise
+
+
+if __name__ == "__main__":
+    token = DISCORD_TOKEN
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    last_messages = 5
+    client = discord.Client(intents=intents)
+
+
+
+    @client.event
+    async def on_ready():
+        print(f'We have logged in as {client.user}')
+
+    @client.event
+    async def on_message(message):
+        channels = ["general"]
+
+        if str(message.channel) in channels:
+            if message.content == "!history":
+                messages = [message.content async for message in message.channel.history(limit=last_messages)]
+
+                await message.channel.send(f"Last {last_messages} messages:")
+                await message.channel.send(messages)
+
+    client.run(token)
